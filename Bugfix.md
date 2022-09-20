df8f0c715a0cf218d84fceb3695d8fe4f006d737
Janan <71359987+Jananzzzz@users.noreply.github.com>
2022-09-20 21:51:25 +0800

    finishing data visualization

--- Bugfix.md
+++ Bugfix.md
@@ -1,8 +1,15 @@
 # Bugfix and Skills
----
-## Vscode Extension: 
-### font setting:
-set the font to consolas
-## codec can't decode bytes in position 2-3: truncated \UXXXXXXXX escape
 
+## Vscode Extension
+Markdown Preview Github Styling <br>
+`Github`
+
+## codec can't decode bytes in position 2-3: truncated \UXXXXXXXX escape
+File path error: python regard "C:\User\16591" as string instead of a file path <br>
+Instead, we use `slash` replace `backslash`: "C:/User/16591/" <br>
+
+## <span style="color:red">   length limit (not fixed yet)  <span>
+<div style="color:red"> 
+the output length has a limit in terminal  
+</div>
 
--- DataVisualizing.py
+++ DataVisualizing.py
@@ -2,32 +2,50 @@ import csv
 
 import matplotlib.pyplot as plt
 
-
-with open("C:/Users/16591/Desktop/MachineLearningLab/Challenge2_LongtailedDistributionEmotionRecognition/code/dataset/fer2013A.csv", 'r') as f:
+x0 = 0
+x1 = 0
+x2 = 0
+x3 = 0
+x4 = 0
+x5 = 0
+x6 = 0
+with open("C:/Users/16591/Desktop/MachineLearningLab/Challenge2_LongtailedDistributionEmotionRecognition/code/dataset/fer2013B.csv", 'r') as f:
     reader = csv.reader(f)
     list_csv = list(reader)
-    print(list_csv)
+    for line in list_csv:
+     if line[0] == 'emotion':
+          {}
+     elif line[0] == '0':
+          x0 = x0 + 1
+     elif line[0] == '1':
+          x1 = x1 + 1
+     elif line[0] == '2':
+          x2 = x2 + 1
+     elif line[0] == '3':
+          x3 = x3 + 1
+     elif line[0] == '4':
+          x4 = x4 + 1
+     elif line[0] == '5':
+          x5 = x5 + 1
+     elif line[0] == '6':
+          x6 = x6 + 1
 
-    #for line in list_csv:
-     #   print(line[0])
+y = [x0, x1, x2, x3, x4, x5, x6]
+Y = sorted(y)
+X = [0, 1, 2, 3, 4, 5, 6]
 
+for i in range(7):
+     for j in range(7):
+          if Y[i] == y[j]:
+               X[i] = j
 
-x = [1,1,2,3,3,5,7,8,9,10,
-     10,11,11,13,13,15,16,17,18,18,
-     18,19,20,21,21,23,24,24,25,25,
-     25,25,26,26,26,27,27,27,27,27,
-     29,30,30,31,33,34,34,34,35,36,
-     36,37,37,38,38,39,40,41,41,42,
-     43,44,45,45,46,47,48,48,49,50,
-     51,52,53,54,55,55,56,57,58,60,
-     61,63,64,65,66,68,70,71,72,74,
-     75,77,81,83,84,87,89,90,90,91
-     ]
-plt.hist(x, bins = 10)
+plt.rcParams["figure.figsize"] = [15.00, 5.00]
+
+default_x_ticks = range(len(X))
+plt.xticks(default_x_ticks, X)
+plt.plot(default_x_ticks, Y)
 plt.show()
 
 
-# read the data from .csv files 
-
-
-# plot a histogram of the data
+#plt.hist(Y)
+#plt.savepic("figure: 1")
